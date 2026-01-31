2cd7867a5991c87644cc4ec74b4866ab6b63d338
AT13xe <845043294@qq.com>
2026-01-31 13:22:33 +0800

    update

---
+++ js/rules_direct.js
@@ -0,0 +1,14 @@
+window.RULES_DIRECT = {
+    "/blog": {
+        "url": "https://blog.at13xe.top",
+        "expired_at": ""
+    },
+    "/esa": {
+        "url": "https://tianchi.aliyun.com/specials/promotion/freetier/esa?taskCode=25254&recordId=b12555cf3ba0f480b3ac34726a281f96",
+        "expired_at": ""
+    },
+    "/list": {
+        "url": "https://aamdeldtxyoh.ap-northeast-1.clawcloudrun.com",
+        "expired_at": ""
+    }
+};
---
+++ js/rules_intermediate.js
@@ -0,0 +1,6 @@
+window.RULES_INTERMEDIATE = {
+  "/example": {
+        "url": "https://example.com",
+        "expired_at": "2026-01-29T14:52:00.000Z"
+    }
+};
